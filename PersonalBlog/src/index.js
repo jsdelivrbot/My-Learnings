dd7ee661a230673fc2e77577d69fde8f99221db4
meghna-saxena <meghna.s1906@gmail.com>
2017-05-08 21:30:52 +0530

    created route path on DOM

--- PersonalBlog/src/index.js
+++ PersonalBlog/src/index.js
@@ -2,14 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import {BrowserRouter, Route}  from 'react-router-dom';  
 
 import App from './components/app';
 import reducers from './reducers';
 
+//Creating test components -
+class Hello extends React.component {
+	render() {return  <div>Hello!</div>}
+}
+
+class Goodbye extends React.component {
+	render() {return  <div>Goodbye!</div>}
+}
+
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+	<Provider store={createStoreWithMiddleware(reducers)}>
+		<BrowserRouter>
+			<Route path="/hello" component={Hello} /> //Creating instance of route for 2 test components
+    		<Route path="/goodbye" component={Goodbye} /> 
+    	</BrowserRouter>
+    </Provider>
+ , document.querySelector('.container'));
