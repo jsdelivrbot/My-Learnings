06ded648abd2d96c86be1161810cdf2c85e1187b
meghna-saxena <meghna.s1906@gmail.com>
2017-10-01 02:02:01 +0530

    made an index and new post components using redux form

--- Trials/blog/src/index.js
+++ Trials/blog/src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
-import { BrowserRouter, Route } from 'react-router-dom';
-import PostsIndex from './components/posts_index';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
+import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -13,7 +14,10 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
     	<div>
-    		<Route path="/" component={PostsIndex} />
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/" component={PostsIndex} />
+        </Switch>
     	</div>
     </BrowserRouter>
   </Provider>
--- Trials/blog/src/reducers/reducer_posts.js
+++ Trials/blog/src/reducers/reducer_posts.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import { FETCH_POSTS } from '../actions'
 
 export default function (state={}, action) {
-	switch(action.payload) {
+	switch(action.type) {
 		case FETCH_POSTS:
 			return _.mapKeys(action.payload.data, 'id');
 		default:
